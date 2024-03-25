b82e47578ffbf6d493c018511f5a8d28cb3ac4a5
Animesh Shukla <animeshshukla1518@gmail.com>
2024-03-25 20:20:24 +0530

    feat: get messages functionality implemented

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useAuthContext } from '../../context/AuthContext'
+import useConversation from '../../store/useConversations';
+import { extractTime } from '../../utils/extractTime';
 
-function Message() {
+function Message({message}) {
+  const {authUser}=useAuthContext();
+  const {selectedConversation}= useConversation();
+  const fromMe=message.senderId===authUser._id;
+  const chatClassName = fromMe? 'chat chat-end':'chat chat-start';
+  const time = extractTime(message.createdAt)
+  const profilePic = fromMe? authUser.profilePic : selectedConversation.profilePic;
+  console.log(profilePic);
+  console.log(authUser);
+  const bgColor = fromMe? 'bg-slate-900':"";
   return (
-    <div className="chat chat-end">
+    <div className={chatClassName}>
   <div className="chat-image avatar">
     <div className="w-10 rounded-full">
-      <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+      <img alt="User Avatar" src={profilePic} />
     </div>
   </div>
-  <div className="chat-bubble">I hate you!</div>
-  <div className="chat-footer opacity-50">12:46</div>
+  <div className={`chat-bubble text-whit ${bgColor}`}>{message.message}</div>
+  <div className="chat-footer opacity-50">{time}</div>
 </div>
   )
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../loading_skeletons/MessageSkeleton';
 
 function Messages() {
+  const {messages,loading}=useGetMessages();
+  const lastMessageRef = useRef();
+  useEffect(()=>{
+    setTimeout(() => {
+      lastMessageRef.current.scrollIntoView({behavior:"smooth"});
+    }, 100);
+    
+  },[messages]);
   return (
     <div className='px-4 flex-1 overflow-auto'>
-      <Message/>
-      <Message/>
-      <Message/>
+      {!loading && messages.length>0 && messages.map((message)=>(
+        <div key={message._id} ref={lastMessageRef}>
+          <Message message={message}/>
+        </div>
+      ))}
+      {loading && [...Array(5)].map((_,idx)=>(<MessageSkeleton key={idx}/>))}
+
+      {!loading && messages.length===0 && (
+        <p className='text-center'>Send a message to start conversation</p>
+      )}
     </div>
   )
 }
