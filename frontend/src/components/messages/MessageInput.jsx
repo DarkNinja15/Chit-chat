a29ccfe815dd48221440e1a685002d93a8edb70b
Animesh Shukla <animeshshukla1518@gmail.com>
2024-03-25 18:49:52 +0530

    feat: added send message functionality

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LuSend } from "react-icons/lu";
+import useSendMessage from '../../hooks/useSendMessage';
 
 function MessageInput() {
+  const [message,setMessage]=useState("");
+  const {loading,sendMessage}= useSendMessage();
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    if(!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
+
   return (
-    <form className="px-4 my-3">
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
         <div className='w-full relative'>
-            <input type="text" placeholder='Send a message' className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white' />
+            <input type="text" placeholder='Send a message' className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white' value={message} onChange={(e)=>setMessage(e.target.value)}/>
             <button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3 cursor-pointer'>
-        <LuSend />
+        {loading? <span className='loading loading-spinner'></span>:<LuSend />}
         </button>
         </div>
         
     </form>
   )
 }
-
-
 
 export default MessageInput
