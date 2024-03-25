c3ac62760773717d3e91f25b9dd1e985e679efe7
Animesh Shukla <animeshshukla1518@gmail.com>
2024-03-25 20:35:24 +0530

    feat: added search functionality

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CiSearch } from "react-icons/ci";
+import useConversation from '../../store/useConversations';
+import useGetConversations from '../../hooks/useGetConversations';
+import toast from 'react-hot-toast';
 
 function SearchInput() {
+  const [search,setSearch]=useState("");
+  const {setSelectedConversation}=useConversation();
+  const {conversations}= useGetConversations();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(!search) return;
+
+    const conversation = conversations.find((c)=>(
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    ));
+
+    if(conversation){
+      setSelectedConversation(conversation);
+      setSearch('');
+    }
+    else{
+      toast.error('No such user found');
+    }
+  }
+
   return (
-    <form className='flex items-center gap-2'>
-        <input type="text" placeholder='Search...' className='input input-bordered rounded-full'/>
+    <form className='flex items-center gap-2' onSubmit={handleSubmit}>
+        <input type="text" placeholder='Search...' className='input input-bordered rounded-full' value={search} onChange={(e)=>setSearch(e.target.value)}/>
         <button type='submit' className='btn btn-circle bg-gray-400 text-white hover:bg-gray-400'>
         <CiSearch className='w-6 h-6 outline-none'/>
         </button>
